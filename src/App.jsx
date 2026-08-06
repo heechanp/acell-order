5a656042c4969dde6b6206e1a03f1bded28e52b9
Heechan Park <gmlcks2305@gmail.com>
2026-08-06 14:47:32 +0900

    명세서 test

--- src/App.jsx
+++ src/App.jsx
@@ -1086,7 +1086,7 @@ const target = pdfOrderStatementRef.current;
         } else {
   // 긴 명세서를 페이지별 이미지로 잘라서 저장
   const sourcePageHeight =
-    Math.floor((printableHeight / printableWidth) * image.width);
+  Math.floor((printableHeight / printableWidth) * image.width) - 50;
 
   let sourceY = 0;
   let pageIndex = 0;
